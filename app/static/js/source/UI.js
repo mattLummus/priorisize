842ed12a1637126d62e1a727261f5a6717932099
Matt Lummus <matthew.w.lummus@gmail.com>
2014-03-21 21:35:50 -0500

    css update

--- app/static/js/source/UI.js
+++ app/static/js/source/UI.js
@@ -6,22 +6,44 @@
 
   function initialize(){
     $(document).foundation();
-    $('#forms').hide();
     fixPanel();
+    $('#forms').hide();
+    $('.navForm').hide();
+    $('#logButton').click(toggleReg);
+    $('#regButton').click(toggleLog);
     $('#toggle').click(toggleForms);
+  }
+
+  function toggleReg(){
+    $('#registerForm').toggle();
+    event.preventDefault();
+  }
+
+  function toggleLog(){
+    $('#loginForm').toggle();
+    event.preventDefault();
   }
 
   function toggleForms(){
     $('#forms').fadeToggle('slow', 'linear');
+    event.preventDefault();
   }
 
   //OVERRIDES FOUNDATION CSS
   function fixPanel(){
-    $('.navPanel').css('background-color', '#f1fdfe');
-    $('.navPanel').css('color', 'black');
-    $('#navTitle').css('color', 'white');
-    $('#navTitle').css('font-size', '20px');
-    $('#navTitle').css('background-color', '#007095');
+    $('.navPanel').css('background-color', '#007095');
+    $('.navPanel').css('font-size', '20px');
+    $('.navPanel').css('color', 'white');
+    $('.navPanel').css('box-shadow', 'inset 0 0 10px #000000');
+    $('.navPanel').css('text-shadow', '3px 3px 0px rgba(28, 171, 255, 1)');
+    $('.navPanel').css('width', '200px');
+    $('.navForm').css('background-color', '#007095');
+    $('.navForm').css('font-size', '20px');
+    $('.navForm').css('color', 'white');
+    $('.navForm').css('box-shadow', 'inset 0 0 10px #000000');
+    $('.navForm').css('width', '1038px');
+    $('.navForm').css('float', 'right');
+    $('.name').css('height', '80px');
   }
 
 
