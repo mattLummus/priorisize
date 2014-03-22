cf90b6ef99684644bce98db57b8c193ec57bc02a
Matt Lummus <matthew.w.lummus@gmail.com>
2014-03-21 21:44:40 -0500

    installing login/out, register

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -14,9 +14,13 @@ module.exports = function(req, res, next){
 
 function load(app, fn){
   var home = require('../routes/home');
+  var users = require('../routes/users');
 
   app.get('/', d, home.index);
   app.get('/admin', d, home.admin);
+  app.post('/register', d, users.create);
+  app.post('/login', d, users.login);
+  app.post('/logout', d, users.logout);
   console.log('Routes Loaded');
   fn();
 }
