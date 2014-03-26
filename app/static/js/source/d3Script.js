3784e742da91d8df818a301d567d7344bf66d632
Matt Lummus <matthew.w.lummus@gmail.com>
2014-03-26 15:40:41 -0500

    4pm commit

--- app/static/js/source/d3Script.js
+++ app/static/js/source/d3Script.js
@@ -10,7 +10,6 @@
     $(document).foundation();
   }
 
-
   var width = 1000,
       height = 900,
       root;
@@ -46,6 +45,21 @@
         .links(links)
         .start();
 
+    //For collision detection
+    //This has been added to the original file. If buggy, this may be the problem.
+  /*
+    force.on('tick', function(e) {
+      var q = d3.geom.quadtree(nodes),
+          i = 0,
+          n = nodes.length;
+
+      while (++i < n){ q.visit(collide(nodes[i]));}
+
+      svg.selectAll('circle')
+          .attr('cx', function(d) { return d.x; })
+          .attr('cy', function(d) { return d.y; });
+    });
+*/
     // Update links.
     link = link.data(links, function(d) { return d.target.id; });
 
@@ -119,5 +133,30 @@
     return nodes;
   }
 
-
+/*
+  function collide(node) {
+    var r = node.radius + 16,
+        nx1 = node.x - r,
+        nx2 = node.x + r,
+        ny1 = node.y - r,
+        ny2 = node.y + r;
+    return function(quad, x1, y1, x2, y2) {
+      if (quad.point && (quad.point !== node)) {
+        var x = node.x - quad.point.x,
+            y = node.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y),
+            r = node.radius + quad.point.radius;
+        if (l < r) {
+          l = (l - r) / l * .5;
+          node.x -= x *= l;
+          node.y -= y *= l;
+          quad.point.x += x;
+          quad.point.y += y;
+        }
+      }
+      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+    };
+  }
+*/
+//end of document
 })();
